895aeed57549627af978c1c4431276a21d411f69
Seaf Aliyan <codo47@gmail.com>
2021-11-29 11:50:26 +0200

    feat: use helmet

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
-const path = require('path');
+const helmet = require('helmet');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 app.use('/cards', cards);
 app.use('/users', users);
 
 app.get('*', (req, res) => {
-  res.status(404).send({
-    message: 'Requested resource not found' });
+  res.status(404).send({ message: 'Requested resource not found' });
 });
 
 app.listen(PORT, () => {
